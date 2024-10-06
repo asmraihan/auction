da93574b1c9015ad0e689d946ef1133206d5b911
asmraihan <asmraihanctg14@gmail.com>
2024-10-06 19:37:41 +0600

    Add environment configuration and database setup

---
+++ src/database/schema.ts
@@ -0,0 +1,17 @@
+import {
+    pgTable,
+    serial,
+    text,
+    timestamp,
+} from 'drizzle-orm/pg-core';
+
+
+export const users = pgTable("user", {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    name: text("name"),
+    email: text("email").notNull(),
+    emailVerified: timestamp("emailVerified", { mode: "date" }),
+    image: text("image"),
+  });
